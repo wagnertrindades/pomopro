3d020ac84ef8a17e6487d5108264cb01498d3179
Wagner Trindade <wtrindades@hotmail.com>
2015-08-02 18:06:17 -0300

    Start app core

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'project.core'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,7 +68,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
