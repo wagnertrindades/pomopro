a7fc3a83ec8c1a0c8a81cc8c974c19feaefe6668
Wagner Trindade <wtrindades@hotmail.com>
2015-08-04 16:55:17 -0300

    Add logout

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    # Login
     url(r'^$', 'django.contrib.auth.views.login', 
         {'template_name' : 'home.html'}, name='home'),
+    # Logout
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page' : 'core:home'}, name='logout'),
     url(r'^timer/$', 'project.core.views.timer', name='timer'),
 )
